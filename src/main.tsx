86ab4c93c35c987fe0aac0e9c11213aaa8ed7be9
Ahmet IZGI <dev.ahmetizgi@gmail.com>
2023-07-09 15:07:11 +0300

    feat: vite initialized

--- src/main.tsx
+++ src/main.tsx
@@ -1,20 +1,10 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App.tsx";
-import { ConfigProvider, theme } from "antd";
-import "./assets/style/style.scss";
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.tsx'
+import './index.css'
 
-ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <ConfigProvider
-      theme={{
-        algorithm: theme.darkAlgorithm,
-        token: {
-          fontFamily: "Epilogue",
-        },
-      }}
-    >
-      <App />
-    </ConfigProvider>
-  </React.StrictMode>
-);
+    <App />
+  </React.StrictMode>,
+)
