85c072f33c7b86b68f44a0568438f50930e4c318
Ahmet IZGI <dev.ahmetizgi@gmail.com>
2023-07-09 16:11:36 +0300

    feat: absolute path added

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,9 @@
+import { Navbar } from "@/components";
+
 function App() {
   return (
     <div>
+      <Navbar />
       <h1 className="text-3xl font-bold underline text-red-400">Hello world!</h1>
     </div>
   );
